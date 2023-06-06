62195bd0a26dc105035732d5706edd7c3f3b2c8b
LORDyyyyy <mlord6271@gmail.com>
2023-06-06 20:01:16 +0000

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(f'0{i}' if i < 9 else f'{i}', end = '')
+    print('0{ii}'.format(ii=i) if i < 9 else f'{i}', end = '')
     print(', ' if i != 99 else '\n', end = '')
