3d46a9864ff3d65cff5ea321a333127f0ffe60d4
LORDyyyyy <mlord6271@gmail.com>
2023-06-14 20:59:08 +0300

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/use/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max = list(a_dictionary.values())[0]
+    score = ""
+    for key, value in a_dictionary.items():
+        if max < value:
+            score = key
+    return score
