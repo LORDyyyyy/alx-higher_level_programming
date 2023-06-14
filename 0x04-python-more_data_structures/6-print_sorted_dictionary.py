af71197a0328dcd6e8c67b9b64a9988c37e8ca80
LORDyyyyy <mlord6271@gmail.com>
2023-06-14 20:31:21 +0300

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/pthon3
+def print_sorted_dictionary(a_dictionary):
+    dic = a_dictionary.copy()
+    keys = list(dic.keys())
+    keys.sort()
+    sdic = {i: dic[i] for i in keys}
+    for key, value in sdic.items():
+        print(f"{key}: {value}")
