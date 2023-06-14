17c3c4525baa1aa5dee8dd47f16036317d86307b
LORDyyyyy <mlord6271@gmail.com>
2023-06-14 21:00:09 +0300

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
+    if a_dictionary is None:
         return None
     max = list(a_dictionary.values())[0]
     score = ""
