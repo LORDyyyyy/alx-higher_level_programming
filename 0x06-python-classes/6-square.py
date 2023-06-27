8c02bce3aa0550592c414c99aeccc087a23e80cb
LORDyyyyy <mlord6271@gmail.com>
2023-06-27 23:53:23 +0300

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,7 +28,7 @@ class Square:
         elif position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
 
-        self.__position = position
+        self.__position = position.copy()
 
     @property
     def size(self):
