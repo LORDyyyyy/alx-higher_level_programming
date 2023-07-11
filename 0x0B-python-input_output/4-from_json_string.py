648a07ae228edd0f04dbf133f676d071696bbfee
LORDyyyyy <mlord6271@gmail.com>
2023-07-11 22:03:05 +0300

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""import json"""
+import json
+
+
+def from_json_string(my_str):
+    """ a function that returns an object represented by a JSON string"""
+
+    return (json.loads(my_str))
