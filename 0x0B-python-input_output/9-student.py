54f3b5aea311ecbb82a9fddd05e4058e69bee347
LORDyyyyy <mlord6271@gmail.com>
2023-07-11 23:27:50 +0300

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,9 +4,13 @@ class Student:
     """ a student class"""
 
     def __init__(self, first_name, last_name, age):
+        """inti """
+
         self.age = age
         self.first_name = first_name
         self.last_name = last_name
 
     def to_json(self):
+        """retrieves a dictionary representation"""
+
         return (self.__dict__)
