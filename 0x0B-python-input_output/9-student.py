225fb54801941d066cfa77047666b6c8d2c3606f
LORDyyyyy <mlord6271@gmail.com>
2023-07-11 23:26:39 +0300

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""import json"""
+import json
+
+
+class Student:
+    """ a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def to_json(self):
+        return (self.__dict__)
