f13077fcd24225a5d26ca0f75049d813d7a9fa9e
LORDyyyyy <mlord6271@gmail.com>
2023-07-11 23:32:33 +0300

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""here goes the class"""
+
+
+class Student:
+    """ a student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """inti """
+
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation"""
+        if attr is None:
+            return (self.__dict__)
+        else:
+            dict = {}
+            for i in attrs:
+                if hasatter(self, i):
+                    dict[i] = getattr(self, nm)
+            return (dict)
