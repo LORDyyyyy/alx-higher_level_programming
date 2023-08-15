abb4adeaceb5328b7f718f112e9a6855f6f06141
LORDyyyyy <mlord6271@gmail.com>
2023-08-15 22:06:29 +0300

    8-count_89.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creats a second table
+CREAT TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+)
+INSERT INTO second_table VALUES (1, "John", 10), 
+				(2, "Alex", 3), 
+				(3, "Bob", 14), 
+				(1, "George", 8);
