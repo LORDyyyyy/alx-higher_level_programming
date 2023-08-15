7cc2c09264bf60808884645ced4c7a14927385c5
LORDyyyyy <mlord6271@gmail.com>
2023-08-15 22:13:24 +0300

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,13 @@
 -- creats a second table
-CREAT TABLE IF NOT EXISTS second_table (
+DELIMITER //
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
-)
-INSERT INTO second_table VALUES (1, "John", 10), 
-				(2, "Alex", 3), 
-				(3, "Bob", 14), 
-				(1, "George", 8);
+)//
+
+
+INSERT INTO second_table (id, name, score) 
+VALUES  (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (1, 'George', 8)//
+
+DELIMITER ;
