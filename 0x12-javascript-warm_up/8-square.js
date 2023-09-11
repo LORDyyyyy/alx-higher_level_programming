9bacea161d48095c2a4396a24b90fcfc65975365
LORDyyyyy <mlord6271@gmail.com>
2023-09-11 14:58:59 +0300

    Task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const [,, argument] = process.argv;
+
+const size = parseInt(argument);
+
+if (!isNaN(size)) {
+  for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+} else {
+  console.log('Missing size');
+}
