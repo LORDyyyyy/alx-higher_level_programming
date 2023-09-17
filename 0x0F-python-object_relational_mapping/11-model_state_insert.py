0f34d2503d00b20343edd133fbd158b81671074e
LORDyyyyy <mlord6271@gmail.com>
2023-09-17 17:01:49 +0300

    7-model_state_fetch_all.py

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Here goes the code"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine(
+            f'mysql://{username}:{password}@localhost:3306/{database}')
+    Base.metadata.bind = engine
+
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    
+    new_state = State(name="Louisiana")
+
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
