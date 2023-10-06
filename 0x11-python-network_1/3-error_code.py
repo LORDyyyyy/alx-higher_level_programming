0c6812cc0f52bc21b08cf4cfc296f18513cfe226
LORDyyyyy <mlord6271@gmail.com>
2023-10-06 22:19:11 +0300

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,12 +6,10 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    
+
     try:
-        # Send a request to the URL
         with urllib.request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
 
     except urllib.error.HTTPError as e:
-        # Handle HTTP errors
         print(f"Error code: {e.code}")
