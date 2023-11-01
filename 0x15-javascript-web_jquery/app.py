4bb94056c5e5782768338c71367080ebcd0b0b9b
LORDyyyyy <mlord6271@gmail.com>
2023-11-01 20:38:56 +0200

    1-script.js

--- 0x15-javascript-web_jquery/app.py
+++ 0x15-javascript-web_jquery/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """ """
-    return render_template('0-main.html')
+    return render_template('1-main.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
